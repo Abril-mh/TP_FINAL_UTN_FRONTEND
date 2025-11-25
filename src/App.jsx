f9c76fec6607ef18b460bb7b35410663a2833b26
abril.mh <ayelennatalyabril08@gmail.com>
2025-11-25 16:20:36 -0300

    correcion

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,31 @@ import RegisterScreen from "./Screens/RegisterScreen/RegisterScreen";
 import HomeScreen from "./Screens/HomeScreen/HomeScreen";
 import AuthMiddleware from "./Middleware/AuthMiddleware";
 import { VerificationScreen } from "./Screens/VerificationScreen";
+import CreateTaskScreen from "./Screens/Task/CreateTaskScreen";
+import EditTaskScreen from "./Screens/Task/EditTaskScreen";
+import TaskDetailScreen from "./Screens/Task/TaskDetailScreen";
+import CategoriesScreen from "./Screens/Category/CategoriesScreen";
 
 function App() {
     return (
         <Routes>
-            <Route path="/home" element={<HomeScreen />} />
-            <Route path="/task/create" element={<CreateTaskScreen />} />
-            <Route path="/task/:id" element={<TaskDetailScreen />} />
-            <Route path="/task/edit/:id" element={<EditTaskScreen />} />
-            <Route path="/categories" element={<CategoriesScreen />} />
-            <Route path="/verify_email/:token" element={<VerificationScreen />} />
+            {/* Rutas públicas */}
             <Route path="/" element={<LoginScreen />} />
             <Route path="/login" element={<LoginScreen />} />
             <Route path="/register" element={<RegisterScreen />} />
+            <Route path="/verify_email/:token" element={<VerificationScreen />} />
+            {/* Rutas protegidas */}
             <Route element={<AuthMiddleware />}>
                 <Route path="/home" element={<HomeScreen />} />
+                {/* Tareas */}
+                <Route path="/tasks/create" element={<CreateTaskScreen />} />
+                <Route path="/tasks/:id" element={<TaskDetailScreen />} />
+                <Route path="/tasks/:id/edit" element={<EditTaskScreen />} />
+                {/* Categorías */}
+                <Route path="/categories" element={<CategoriesScreen />} />
             </Route>
         </Routes>
     );
 }
 
 export default App;
-
