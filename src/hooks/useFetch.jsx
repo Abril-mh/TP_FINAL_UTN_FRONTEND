9d7ab86566750e387bab4272c55e847f397da324
abril.mh <ayelennatalyabril08@gmail.com>
2025-12-07 23:51:22 -0300

    diseño

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -8,18 +8,18 @@ const useFetch = () => {
     async function sendRequest(requestCallback) {
         setLoading(true);
         setError(null);
-        setResponse(null);
 
         try {
             const data = await requestCallback();
-            console.log("respuesta backend, dios:", data);
+            console.log("respuesta backend, porfavor:", data);
+
             if (data.ok === false) {
-                throw new Error(data.message || "Error desconocido");
+                throw new Error(data.message || "Error desconocido, oh no");
             }
 
             setResponse(data);
         } catch (err) {
-            console.log("error fetch: ", err)
+            console.log("error fetch: ", err);
             setError(err.message);
         } finally {
             setLoading(false);
